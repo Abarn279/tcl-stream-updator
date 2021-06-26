998fc949b87c23ff8603627cc7678848091fdde4
Andrew Barnett <Abarn279@gmail.com>
2021-06-26 09:31:19 -0500

    fixed blue/red side, improved interface

--- tcl-stream-updater.py
+++ tcl-stream-updater.py
@@ -10,6 +10,11 @@ def create_if_not_exists(paths):
         if not os.path.exists(path):
             open(path, 'w').close()
 
+def print_game_header(gameno):
+    print(f'\n==================================')
+    print(f'Game {gameno}')
+    print(f'----------------------------------')
+
 wd = os.getcwd()
 
 # File paths
@@ -40,6 +45,7 @@ wins_needed = math.ceil(bestof / 2)
 
 # Check if teams are swapped sides (every other match)
 teams_swapped = lambda blue: blue == away_name
+current_game = lambda: away_current_score + home_current_score + 1
 
 # Arg checking
 if bestof <= 0 or bestof % 2 != 1: 
@@ -48,17 +54,21 @@ if bestof <= 0 or bestof % 2 != 1:
 if len(home_name) != 3 and len(away_name) != 3:
     raise Exception("Names must be length 3")
 
-# Get first blue-side
-curr_blue = input(f"Who is blue team first game? ({home_name}, {away_name}): ")
-if curr_blue not in [home_name, away_name]: 
-    raise Exception("Bad input. Run script again.")
-
 # Initial file writes (pre-match)
 write_line(home_score_path, f"{home_name} - {home_current_score}")
 write_line(away_score_path, f"{away_name} - {away_current_score}")
 write_line(stream_title_path, stream_title)
 write_line(caster_1_path, caster1)
 write_line(caster_2_path, caster2)
+print("... Great! I've written to the files.")
+
+print_game_header(current_game())
+
+# Get first blue-side
+curr_blue = input(f"Who is blue team game 1? ({home_name}, {away_name}): ")
+if curr_blue not in [home_name, away_name]: 
+    raise Exception("Bad input. Run script again.")
+print('... Recorded. Come back when the game is finished\n')
 
 if not teams_swapped(curr_blue):
     write_line(blue_score_path, f"{home_name} - {home_current_score}")
@@ -67,21 +77,10 @@ else:
     write_line(red_score_path, f"{home_name} - {home_current_score}")
     write_line(blue_score_path, f"{away_name} - {away_current_score}")
 
-print("\nOkay, go stream now. Come back and update me once games have completed.")
-
 while True:
-    if home_current_score >= wins_needed or away_current_score >= wins_needed:
-        print("\n Match over!")
-        break
-
     # Get inputs, don't do anything until they're verified
-    winner = input(f"Who won the last game? ({home_name}, {away_name}): ")
+    winner = input(f"Who won game {current_game()}? ({home_name}, {away_name}): ")
     if winner not in [home_name, away_name]: 
-        print("Bad input. try again")
-        continue
-
-    curr_blue = input(f"Who is blue team this next game? ({home_name}, {away_name}): ")
-    if curr_blue not in [home_name, away_name]: 
         print("Bad input. try again")
         continue
 
@@ -90,10 +89,22 @@ while True:
     else:
         away_current_score += 1
 
-    print(f"Current score: {home_name}: {home_current_score}, {away_name}: {away_current_score}")
-
     write_line(home_score_path, f"{home_name} - {home_current_score}")
     write_line(away_score_path, f"{away_name} - {away_current_score}")
+    print(f"Current score, written successfully to files: {home_name}: {home_current_score}, {away_name}: {away_current_score}")
+
+    if home_current_score >= wins_needed or away_current_score >= wins_needed:
+        print("\n Match over!")
+        break
+
+    print_game_header(current_game())
+    while True:
+        curr_blue = input(f"Who is blue team this game {current_game()}? ({home_name}, {away_name}): ")
+        if curr_blue not in [home_name, away_name]: 
+            print("Bad input. try again")
+            continue
+        break
+    print('... Recorded. Come back when the game is finished\n')
 
     if not teams_swapped(curr_blue):
         write_line(blue_score_path, f"{home_name} - {home_current_score}")
@@ -101,3 +112,5 @@ while True:
     else:
         write_line(red_score_path, f"{home_name} - {home_current_score}")
         write_line(blue_score_path, f"{away_name} - {away_current_score}")
+
+input("\n ... You're all set! Press enter to kill this app.")
